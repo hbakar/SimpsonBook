e818d140a432a46ec461491d5472acdff706c346
hbakar <huseyin-bakar@hotmail.com>
2021-12-05 10:46:14 +0300

    Simpson ailesini yakından tanıyalım. v2

--- SimpsonBook/ViewController.swift
+++ SimpsonBook/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    var simpsonArr = [Simpson]()
+    
+    var chosenSimpson : Simpson?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let homer = Simpson(nameInit: "Homer Simpson", jobInit: "Nuclear Safety", imageInit: UIImage(named: "homer")!)
+        
+        let bart = Simpson(nameInit: "Bart Simpson", jobInit: "Student", imageInit: UIImage(named: "bart")!)
+        
+        let maggie = Simpson(nameInit: "Maggie Simpson", jobInit: "Baby", imageInit: UIImage(named: "maggie")!)
+        
+        simpsonArr.append(homer)
+        simpsonArr.append(bart)
+        simpsonArr.append(maggie)
+       
     }
-
-
 }
 
+
+extension ViewController : UITableViewDelegate, UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return simpsonArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text =  self.simpsonArr[indexPath.row].name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        self.chosenSimpson = simpsonArr[indexPath.row]
+        
+       performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC"
+        {
+            let vc = segue.destination as? DetailsVC
+            vc?.chosenSimpson = self.chosenSimpson
+        }
+    }
+}
